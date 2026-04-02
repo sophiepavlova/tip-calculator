8ef37a7475bff2907d9104c14460944f4353c1a3
sophiepavlova <olni.pavlova@gmail.com>
2026-04-02 13:57:12 -0700

    Finish the challenge - my version

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,39 @@ import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [bill, setBill] = useState('');
+  const [satisfaction, setSatisfaction] = useState(0);
+  const [satisfactionSecond, setSatisfactionSecond] = useState(0);
+  const tipsPercetns = Number(satisfaction) + Number(satisfactionSecond);
+  const tipAmount = (Number(bill) * tipsPercetns) / 100;
+  console.log(tipAmount);
+  // console.log(Number(satisfaction));
+  // console.log(Number(satisfactionSecond));
+  // console.log(tipsPercetns);
   return (
     <div>
-      <BillInput />
-      <SelectPercentage text='How did you like the service?' />
-      <SelectPercentage text='How did your friend like the service?' />
-      <Output />
-      <Reset />
+      <BillInput bill={bill} setBill={setBill} />
+      <SelectPercentage
+        text='How did you like the service?'
+        satisfaction={Number(satisfaction)}
+        setSatisfaction={setSatisfaction}
+      />
+      <SelectPercentageSecond
+        text='How did your friend like the service?'
+        satisfactionSecond={satisfactionSecond}
+        setSatisfactionSecond={setSatisfactionSecond}
+      />
+      <Output bill={bill} tipAmount={tipAmount} />
+      <Reset
+        setBill={setBill}
+        setSatisfaction={setSatisfaction}
+        setSatisfactionSecond={setSatisfactionSecond}
+      />
     </div>
   );
 }
 
-function BillInput() {
-  const [bill, setBill] = useState(0);
+function BillInput({ bill, setBill }) {
   return (
     <div>
       <p>How much was the bill?</p>
@@ -27,8 +47,7 @@ function BillInput() {
   );
 }
 
-function SelectPercentage({ text }) {
-  const [satisfaction, setSatisfaction] = useState('Choose the tip');
+function SelectPercentage({ text, satisfaction, setSatisfaction }) {
   return (
     <div>
       <p>{text}</p>
@@ -36,22 +55,51 @@ function SelectPercentage({ text }) {
         value={satisfaction}
         onChange={(e) => setSatisfaction(e.target.value)}
       >
-        <option value='Dissatisfied (0%)'>Dissatisfied (0%)</option>
-        <option value='It was ok (5%)'>It was ok (5%)</option>
-        <option value='It was good (10%)'>It was good (10%)</option>
-        <option value='Absolutely amazing! (20%)'>
-          Absolutely amazing! (20%)
-        </option>
+        <option value='0'>Dissatisfied (0%)</option>
+        <option value='5'>It was ok (5%)</option>
+        <option value='10'>It was good (10%)</option>
+        <option value='20'>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function SelectPercentageSecond({
+  text,
+  satisfactionSecond,
+  setSatisfactionSecond,
+}) {
+  return (
+    <div>
+      <p>{text}</p>
+      <select
+        value={satisfactionSecond}
+        onChange={(e) => setSatisfactionSecond(e.target.value)}
+      >
+        <option value='0'>Dissatisfied (0%)</option>
+        <option value='5'>It was ok (5%)</option>
+        <option value='10'>It was good (10%)</option>
+        <option value='20'>Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
 }
 
-function Output() {
-  return <h1>{`You pay $sum ($bill + $tip)`}</h1>;
+function Output({ bill, tipAmount }) {
+  const sum = bill + tipAmount;
+  return <h1>{`You pay $${sum} ($${bill} + $${tipAmount})`}</h1>;
   // return <h1>{`You pay $${sum} ($${bill} + $${tip})`}</h1>;
 }
 
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ setBill, setSatisfaction, setSatisfactionSecond }) {
+  return (
+    <button
+      onClick={() => {
+        setBill(0);
+        setSatisfaction(0);
+        setSatisfactionSecond(0);
+      }}
+    >
+      Reset
+    </button>
+  );
 }
